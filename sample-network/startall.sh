9cdf4f7651f0de00b6d954c869ac099b956df9e6
k0ezeN <andyguerin@Andy.local>
2023-04-05 09:10:02 +0200

    added scripts

--- sample-network/startall.sh
+++ sample-network/startall.sh
@@ -4,3 +4,5 @@ kubectl create -f storageClass.yaml
 kubectl apply -f pv0.yaml
 kubectl apply -f pv1.yaml
 kubectl apply -f pv2.yaml
+
+network up
---
+++ sample-network/stopall.sh
@@ -0,0 +1,7 @@
+network down 
+
+kubectl delete pv task-pv-volume-org0
+kubectl delete pv task-pv-volume-org1
+kubectl delete pv task-pv-volume-org2
+
+kubectl delete sc ibm-file-gold
